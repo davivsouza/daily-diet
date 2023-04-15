beb27081075836408139608b85690b68452c6eed
Davi Vasconcelos Souza <davivasconcelossouza7@gmail.com>
2023-04-15 21:31:17 +0000

    feat: fontsLoaded loading

--- App.tsx
+++ App.tsx
@@ -1,22 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { Home } from './src/screens/Home';
-import { ThemeProvider } from 'styled-components';
 import {
   useFonts,
   NunitoSans_400Regular,
-  NunitoSans_600SemiBold
+  NunitoSans_700Bold
 } from '@expo-google-fonts/nunito-sans'
 import theme from './src/theme'
+import { ThemeProvider } from 'styled-components';
+import { StatusBar } from 'expo-status-bar';
+import { Loading } from '@components/Loading';
+import { Home } from '@screens/Home';
 
 export default function App() {
 
   const [fontsLoaded] = useFonts({
     NunitoSans_400Regular,
-    NunitoSans_600SemiBold
+    NunitoSans_700Bold
   })
   return (
     <ThemeProvider theme={theme}>
-      <Home/>
+      {fontsLoaded ? <Home/> : <Loading/>}
       <StatusBar style="dark" translucent/>
     </ThemeProvider>
   );
